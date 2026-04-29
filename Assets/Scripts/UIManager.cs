33f6acda978e2c55c42553eb49890a2f0306e82c
曾朗 <3449930887@qq.com>
2026-04-29 23:56:19 +0800

    添加玩家2

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,18 +6,20 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("玩家总数")]
+    public int PlayerCount;
+
     [Header("玩家")]
-    public PlayerController player;
+    public List<PlayerController> players = new List<PlayerController>();
 
     [Header("血条")]
-    public Slider bloodBar;
+    public List<Slider> bloodBars = new List<Slider>();
 
     [Header("体力条")]
-    public Slider staminaBar;
+    public List<Slider> staminaBars = new List<Slider>();
 
     [Header("子弹")]
-    public TMP_Text ammoText;  
-
+    public List<TMP_Text> ammoTexts = new List<TMP_Text>();
     void Start()
     {
 
@@ -31,54 +33,65 @@ public class UIManager : MonoBehaviour
     }
 
     private void UpdateBlood()
-{
-    if (player == null || bloodBar == null || player.PlayerStatus == null)
-        return;
+    {
+        for(int i = 0; i < PlayerCount; i++)
+        {
+            if (players[i] == null || bloodBars[i] == null || players[i].PlayerStatus == null)
+                return;
 
-    bloodBar.maxValue = player.PlayerStatus.maxHp;
-    bloodBar.value = player.PlayerStatus.currentHp;
-}
+            bloodBars[i].maxValue = players[i].PlayerStatus.maxHp;
+            bloodBars[i].value = players[i].PlayerStatus.currentHp;
+        }
+        
+    }
 
     private void UpdateStamina()
     {
-        if (player != null && staminaBar != null)
+        for (int i = 0; i < PlayerCount; i++)
         {
-            staminaBar.maxValue = player.maxStamina;
-            staminaBar.value = player.currentStamina;
+            if (players[i] != null && staminaBars[i] != null)
+            {
+                staminaBars[i].maxValue = players[i].maxStamina;
+                staminaBars[i].value = players[i].currentStamina;
+            }
         }
+            
     }
 
     private void UpdateAmmo()
     {
-        if (player == null || ammoText == null) return;
-        
-        int currentAmmo = player.GetCurrentAmmo();
-        int maxAmmo = player.GetMaxAmmo();
-        bool needAmmo = player.IsNeedAmmo();
-        
-        if (needAmmo)
+        for (int i = 0; i < PlayerCount; i++)
         {
-            ammoText.text = $"bullet: {currentAmmo} / {maxAmmo}";
-            
-            // Change text color when ammo is low
-            if (currentAmmo == 0)
+            if (players[i] == null || ammoTexts[i] == null) return;
+
+            int currentAmmo = players[i].GetCurrentAmmo();
+            int maxAmmo = players[i].GetMaxAmmo();
+            bool needAmmo = players[i].IsNeedAmmo();
+
+            if (needAmmo)
             {
-                ammoText.color = Color.red;
-            }
-            else if (currentAmmo <= maxAmmo * 0.2f) // Yellow when ammo is below 20%
-            {
-                ammoText.color = Color.yellow;
+                ammoTexts[i].text = $"bullet: {currentAmmo} / {maxAmmo}";
+
+                // Change text color when ammo is low
+                if (currentAmmo == 0)
+                {
+                    ammoTexts[i].color = Color.red;
+                }
+                else if (currentAmmo <= maxAmmo * 0.2f) // Yellow when ammo is below 20%
+                {
+                    ammoTexts[i].color = Color.yellow;
+                }
+                else
+                {
+                    ammoTexts[i].color = Color.white;
+                }
             }
             else
             {
-                ammoText.color = Color.white;
+                // Unlimited ammo mode
+                ammoTexts[i].text = "Ammo: Infinity";
+                ammoTexts[i].color = Color.cyan;
             }
-        }
-        else
-        {
-            // Unlimited ammo mode
-            ammoText.text = "Ammo: Infinity";
-            ammoText.color = Color.cyan;
         }
     }
 }
