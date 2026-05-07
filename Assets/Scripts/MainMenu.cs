eb0df2be1bd1c8b499a6c1901d7b0f0df5cc3fed
RSCML <454020090@qq.com>
2026-05-07 21:28:49 +0800

    3

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button startButton;
+    public Button quitButton;
+    public Button testButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+        quitButton.onClick.AddListener(ExitGame);
+        testButton.onClick.AddListener(TestGame);
+    }
+
+    public void StartGame()
+    {
+        // Replace "MainGame" with your actual main scene name.
+        SceneManager.LoadScene("MainGame");
+    }
+    public void TestGame()
+    {
+        SceneManager.LoadScene("text");
+    }
+
+    public void ExitGame()
+    {
+        // Quit the game.
+        Application.Quit();
+        // In the Unity Editor, this call does not close play mode; it works in a built game.
+        Debug.Log("Game is exiting.");
+    }
+}
